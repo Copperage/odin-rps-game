eb337a39ed86649463c52d6bbdf1c8e99e82c7f9
copperage <ethanhud6@gmail.com>
2022-02-08 14:01:20 +0000

    Added functionality + bonuses added some extra css too

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DOM selection and init
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
@@ -5,12 +6,19 @@ const scissors = document.querySelector('.scissors');
 let playerScore = 0;
 let computerScore = 0;
 let gameRound = 0;
+let cacheDom = '';
 
+rock.addEventListener('click', () => game('Rock'));
+paper.addEventListener('click', () => game('Paper'));
+scissors.addEventListener('click', () => game('Scissors'));
+
+// Computer randomisation
 function computerChoice() {
 	const randomChoice = ['Rock', 'Paper', 'Scissors'];
 	return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
 
+// RPS Round
 function rpsRound(playerChoice, computerChoice) {
 	console.log(`You selected ${playerChoice}`);
 	console.log(`Computer selected ${computerChoice}`);
@@ -20,7 +28,7 @@ function rpsRound(playerChoice, computerChoice) {
 		(playerChoice === 'Paper' && computerChoice === 'Scissors') ||
 		(playerChoice === 'Scissors' && computerChoice === 'Rock')
 	) {
-		return 'You Lost!';
+		document.querySelector('.decisionText').textContent = 'You lose!';
 		computerScore++;
 		gameRound++;
 	} else if (
@@ -28,7 +36,7 @@ function rpsRound(playerChoice, computerChoice) {
 		(playerChoice === 'Paper' && computerChoice === 'Rock') ||
 		(playerChoice === 'Scissors' && computerChoice === 'Paper')
 	) {
-		return 'You win!';
+		document.querySelector('.decisionText').textContent = 'You win!';
 		playerScore++;
 		gameRound++;
 	} else if (
@@ -36,34 +44,44 @@ function rpsRound(playerChoice, computerChoice) {
 		(playerChoice === 'Paper' && computerChoice === 'Paper') ||
 		(playerChoice === 'Scissors' && computerChoice === 'Scissors')
 	) {
-		return 'Draw!';
+		document.querySelector('.decisionText').textContent = 'Draw!';
 		gameRound++;
 	}
 }
 
+// Game
 function game(playerChoice) {
 	let player = playerChoice;
 	let computer = computerChoice();
 
 	rpsRound(player, computer);
+	document.querySelector('.round').textContent = 'Round' + ' ' + gameRound;
 
-	// if (gameRound === 5 && playerScore > computerScore) {
-	// 	return 'You won the game!';
-	// } else if (gameRound === 5 && computerScore > playerScore) {
-	// 	return 'You lost the game! Better luck next time!';
-	// }
+	if (gameRound === 5) {
+		document.querySelector('.rps').style.visibility = 'hidden';
+		document.querySelector('.restart').style =
+			'font-size: calc(150px + 1.1vw);';
+		document.querySelector('.decisionText').style.visibility = 'hidden';
+	}
+
+	if (gameRound === 5 && playerScore > computerScore) {
+		document.querySelector('.winner').textContent = 'You won the game!';
+	} else if (gameRound === 5 && computerScore > playerScore) {
+		document.querySelector('.winner').textContent = 'You lost the game!';
+	} else if (gameRound === 5 && computerScore === playerScore) {
+		document.querySelector('.winner').textContent = "It's a draw!";
+	}
 }
 
-rock.addEventListener('click', () => game('Rock'));
-paper.addEventListener('click', () => game('Paper'));
-scissors.addEventListener('click', () => game('Scissors'));
-
-/*
-    Set two variables, player and cpu
-    make the player choose a choice in prompt
-    make the cpu choose a random choice
-    compare the two
-    if player wins log you win
-    if cpu wins log you lose
-    if tie then log draw
-*/
+// Reset button
+document.querySelector('.restart').addEventListener('click', () => {
+	playerScore = 0;
+	computerScore = 0;
+	gameRound = 0;
+	document.querySelector('.decisionText').textContent = '';
+	document.querySelector('.round').textContent = 'Round 0';
+	document.querySelector('.rps').style.visibility = 'visible';
+	document.querySelector('.restart').style = 'font-size: calc(40px + 1.1vw);';
+	document.querySelector('.decisionText').style.visibility = 'visible';
+	document.querySelector('.winner').textContent = '';
+});
